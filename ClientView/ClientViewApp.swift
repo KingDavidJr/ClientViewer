f61a417cca9a3a391b32b94a39ecfa3bc017c97e
David <realkd872@gmail.com>
2023-03-22 00:23:25 -0500

    Initial Commit

---
+++ ClientView/ClientViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientViewApp.swift
+//  ClientView
+//
+//  Created by David Amedeka on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ClientViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
